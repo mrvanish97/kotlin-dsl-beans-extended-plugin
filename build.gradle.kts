b9881478a0b60069f2d504bea9e6f27b58d9d472
uonagent <victormushtin@gmail.com>
2021-07-02 20:31:32 +0300

    Copyrights fixed

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2021 mrvanish97 [and others]
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
 import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
